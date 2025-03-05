464fdab87cca4b28de039fd312227de1c4ac81e4
AaniyahE2027 <aaniyah.ellis2027@gbwl.org>
2025-03-05 17:09:45 +0000

    started

--- script.js
+++ script.js
@@ -8,9 +8,16 @@ console.log("Welcome to JavaScript!");
 // This is a SINGLE-LINE COMMENT
 
 // console.log() is a FUNCTION that displays info
-console.log("My name is Katerina");
+console.log("My name is Aaniyah");
 
+//DECLARE a new varible (buying a box)
+let luckyNumber;
 
+//ASSIGN a value to the varible (putting that item in a box)
+luckyNumber= 23;
 
+//DECLARE & ASSIGN in one statement
+let worstNumber = 6;
 
-
+// You can "PASS IN" a VARIBLE into a FUNCTION
+console.log(luckyNumber);
